38496c3dffaea44e69062b26e85b83f8771d5d37
SnowBit <dtshah24022006@gmail.com>
2022-02-19 13:28:04 +0530

    Completed 🌟

--- pages/index.js
+++ pages/index.js
@@ -7,21 +7,21 @@ export default function index() {
   return (
     <>
       <Head>
-      <title>Revelar | URL Revelar</title>
-      <meta name="title" content="Revelar | URL Revelar" />
-      <meta name="description" content="Reveal any shortened links that people send you. It will tell you what the actual link is." />
+        <title>Revelar | URL Revelar</title>
+        <meta name="title" content="Revelar | URL Revelar" />
+        <meta name="description" content="Reveal any shortened links that people send you. It will tell you what the actual link is." />
 
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://revelar.netlify.app/" />
-      <meta property="og:title" content="Revelar | URL Revelar" />
-      <meta property="og:description" content="Reveal any shortened links that people send you. It will tell you what the actual link is." />
-      <meta property="og:image" content="https://revelar.netlify.app/banner.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://revelar.netlify.app/" />
+        <meta property="og:title" content="Revelar | URL Revelar" />
+        <meta property="og:description" content="Reveal any shortened links that people send you. It will tell you what the actual link is." />
+        <meta property="og:image" content="https://revelar.netlify.app/banner.png" />
 
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://revelar.netlify.app/" />
-      <meta property="twitter:title" content="Revelar | URL Revelar" />
-      <meta property="twitter:description" content="Reveal any shortened links that people send you. It will tell you what the actual link is." />
-      <meta property="twitter:image" content="https://revelar.netlify.app/banner.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://revelar.netlify.app/" />
+        <meta property="twitter:title" content="Revelar | URL Revelar" />
+        <meta property="twitter:description" content="Reveal any shortened links that people send you. It will tell you what the actual link is." />
+        <meta property="twitter:image" content="https://revelar.netlify.app/banner.png" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
