d1ad14ca5c3b010807c633ef6f2cf7e60bdf0c4b
SnowBit <codewithsnowbit@gmail.com>
2022-03-31 16:18:48 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="lg:fixed lg:bottom-0 lg:left-0 lg:-rotate-90 lg:mb-9 text-center">
-    <Link href="https://twitter.com/codewithsnowbit">
+    <Link href="https://github.com/codewithsnowbit">
         <a target="_blank" rel="noopener noreferrer">
         <h1 className="text-lg text-center uppercase font-black bg-gradient-to-br from-green-300 via-blue-500 to-purple-600 text-transparent bg-clip-text">SNOWBIT</h1>
         </a>
